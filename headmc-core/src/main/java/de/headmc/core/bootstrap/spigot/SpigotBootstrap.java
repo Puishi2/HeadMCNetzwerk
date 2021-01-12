7c7e3ecb9eb9a79edd80bd53f912c882ad629234
HeadMC <headmcnetzwerk@gmail.com>
2021-01-12 08:23:11 +0100

    Added something

---
+++ headmc-core/src/main/java/de/headmc/core/bootstrap/spigot/SpigotBootstrap.java
@@ -0,0 +1,17 @@
+package de.headmc.core.bootstrap.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotBootstrap extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
