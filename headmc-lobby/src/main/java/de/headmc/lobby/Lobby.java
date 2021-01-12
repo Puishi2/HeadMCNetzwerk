01286eac778c68c84327f3d9a1b0140f00aa8a8a
HeadMC <headmcnetzwerk@gmail.com>
2021-01-12 07:52:46 +0100

    Added something

--- headmc-lobby/src/main/java/de/headmc/lobby/Lobby.java
+++ headmc-lobby/src/main/java/de/headmc/lobby/Lobby.java
@@ -1,16 +1,17 @@
 package de.headmc.lobby;
 
+import de.headmc.core.sql.MySQL;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Lobby extends JavaPlugin {
 
     public static Lobby instance;
 
+    private MySQL mySQL;
+
     @Override
     public void onEnable() {
         instance = this;
-
-        //HUNDESOHN
 
     }
 
